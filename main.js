a1cf1c01989ee19c6855cd7aa1b270e080e92abf
AnnaOmeliuk <annaomeliuk@gmail.com>
2025-12-07 10:12:50 +0200

    menu__container desk styles

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-  
-  const swiper = new Swiper('.swiper', {
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  slidesPerView: 1,
+  breakpoints: {
+    1024: {
+      slidesPerView: 1.6,
+      spaceBetween: 35,
     },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
