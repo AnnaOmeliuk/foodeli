eaaf209a34338f4db01377340b3daa5d6ad7637e
AnnaOmeliuk <annaomeliuk@gmail.com>
2025-12-07 19:48:58 +0200

    menu desktop

--- js/swiper.js
+++ js/swiper.js
@@ -2,15 +2,21 @@ const swiper = new Swiper(".swiper", {
   loop: true,
   slidesPerView: 1,
   breakpoints: {
+
+
     1024: {
-      slidesPerView: 1.6,
+      slidesPerView: 1.3,
       spaceBetween: 35,
     },
+    1440: {
+            slidesPerView: 2,
+      spaceBetween: 35,
+    }
   },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
