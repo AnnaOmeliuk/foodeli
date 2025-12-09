c3334be3e4e7b2412f197115209d4ef844dee248
AnnaOmeliuk <annaomeliuk@gmail.com>
2025-12-10 00:35:26 +0200

    add menu mobile style

--- js/swiper.js
+++ js/swiper.js
@@ -2,26 +2,23 @@ const swiper = new Swiper(".swiper", {
   loop: true,
   slidesPerView: 1,
   breakpoints: {
-
-
-768: {
+    320: {
+          slidesPerView: 0.5
+    },
+    768: {
       slidesPerView: 0.5,
-      spaceBetween: 35
-},
+      spaceBetween: 35,
+    },
 
     1024: {
       slidesPerView: 1.3,
       spaceBetween: 35,
     },
     1440: {
-            slidesPerView: 2,
+      slidesPerView: 2,
       spaceBetween: 35,
-    }
+    },
   },
-  // pagination: {
-  //   el: ".swiper-pagination",
-  //   clickable: true,
-  // },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
